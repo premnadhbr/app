0c3255f354e1e4fe4557df0186180779502a3997
premnadh <premnathbr123@gmail.com>
2023-10-27 19:48:17 +0530

    geolocator done

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,12 +1,13 @@
 import 'package:bloc_mini_project_hive/controller/home/bloc/home_bloc.dart';
 import 'package:bloc_mini_project_hive/controller/switch/bloc/switch_bloc.dart';
 import 'package:bloc_mini_project_hive/utils/constants/constants.dart';
-import 'package:bloc_mini_project_hive/utils/services/notifications.dart';
 import 'package:bloc_mini_project_hive/view/add_student/addScreen.dart';
 import 'package:bloc_mini_project_hive/view/home/home_widget/cardmenu.dart';
 import 'package:bloc_mini_project_hive/view/student_list/students_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 
 import '../../controller/switch/bloc/switch_state.dart';
@@ -20,6 +21,38 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final HomeBloc homeBloc = HomeBloc();
+
+  Position? _currentlocation;
+  late bool servicePermission = false;
+  late LocationPermission permission;
+
+  String currentAdress = "";
+  Future<Position> _getCurrentLocation() async {
+    servicePermission = await Geolocator.isLocationServiceEnabled();
+    if (!servicePermission) {
+      print("Service disabled");
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
+  // geocode the coordinate and convert them into adresses
+  _getAdressFromCoordinates() async {
+    try {
+      List<Placemark> placesMarks = await placemarkFromCoordinates(
+          _currentlocation!.latitude, _currentlocation!.longitude);
+      Placemark place = placesMarks[0];
+      setState(() {
+        currentAdress = "${place.locality}, ${place.country}";
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   void initState() {
@@ -75,7 +108,39 @@ class _HomeScreenState extends State<HomeScreen> {
                             },
                           );
                         },
-                      )
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              const SizedBox(height: 10),
+                              const Text(
+                                'Location Cordinates',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                '${currentAdress}',
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextButton(
+                                  onPressed: () async {
+                                    _currentlocation =
+                                        await _getCurrentLocation();
+                                    await _getAdressFromCoordinates();
+                                    print("${_currentlocation}");
+                                  },
+                                  child: const Text("Get Location"))
+                            ],
+                          )
+                        ],
+                      ),
                     ],
                   ),
                 )
